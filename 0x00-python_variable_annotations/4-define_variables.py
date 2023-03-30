25755f497cd6c0227891c5666f568a032e30b7b1
IamLam163 <olamidealiyu@gmail.com>
2023-03-30 21:10:33 +0100

    annotating different variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''this function defines and annotates specified values '''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
