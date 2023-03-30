e3b95cf7575f1dcb679e1572c02ab1f919403fcb
IamLam163 <olamidealiyu@gmail.com>
2023-03-30 20:47:23 +0100

    lint issues

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 '''
 Python variable annotation
 '''
-
-def addsum(a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
     '''function adds two numbers of type float'''
     return a + b
 
+print(add(1.11, 2.22) == 1.11 + 2.22)
+print(add.__annotations__)
