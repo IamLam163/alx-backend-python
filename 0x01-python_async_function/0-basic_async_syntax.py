8f0f411e893c67b30d8c81bbf539c3766d503687
IamLam163 <olamidealiyu@gmail.com>
2023-04-04 13:32:25 +0100

    removed tests

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,7 +11,3 @@ async def wait_random(max_delay: int = 10) -> float:
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
