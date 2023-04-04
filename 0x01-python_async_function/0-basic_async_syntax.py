b5e8a7f7dcb7616fe90e5c76b6fd7974e8614292
IamLam163 <olamidealiyu@gmail.com>
2023-04-04 13:30:33 +0100

    wait random function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Async in Python
+'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int=10) -> float:
+    '''asynchronous coroutine that waits for a random delay'''
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
+
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+Assync tasks in python
